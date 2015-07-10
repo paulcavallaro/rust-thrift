0ae01ef9f75e91ca85fc9f6c7da37a7cd9ef17ea
Paul Cavallaro <paulcavallaro@gmail.com>
2015-07-10 17:06:57 -0400

    Handle connections closing and deregister and close handles

--- src/server/mioserver.rs
+++ src/server/mioserver.rs
@@ -2,7 +2,7 @@ use mio::*;
 use mio::tcp::*;
 use mio::buf::{ByteBuf, MutByteBuf};
 use mio::util::Slab;
-use std::io;
+use std::io::{Result, Error, ErrorKind};
 
 pub struct MioServer {
     sock: TcpListener,
@@ -28,7 +28,11 @@ impl MioConn {
         }
     }
 
-    fn readable(&mut self, event_loop: &mut EventLoop<MioServer>) -> io::Result<()> {
+    fn closed(&self, event_loop: &mut EventLoop<MioServer>) -> Result<()> {
+        event_loop.deregister(&self.sock)
+    }
+
+    fn readable(&mut self, event_loop: &mut EventLoop<MioServer>) -> Result<()> {
         let mut buf = self.mut_buf.take().unwrap();
 
         match self.sock.try_read_buf(&mut buf) {
@@ -46,10 +50,10 @@ impl MioConn {
 
         // prepare to provide this to writable
         self.buf = Some(buf.flip());
-        event_loop.reregister(&self.sock, self.token.unwrap(), self.interest, PollOpt::edge())
+        event_loop.reregister(&self.sock, self.token.unwrap(), self.interest, PollOpt::edge() | PollOpt::oneshot())
     }
 
-    fn writable(&mut self, event_loop: &mut EventLoop<MioServer>) -> io::Result<()> {
+    fn writable(&mut self, event_loop: &mut EventLoop<MioServer>) -> Result<()> {
         let mut buf = self.buf.take().unwrap();
 
         match self.sock.try_write_buf(&mut buf) {
@@ -81,21 +85,36 @@ impl Handler for MioServer {
 
     fn ready(&mut self, event_loop: &mut EventLoop<Self>, token: Token,
              events: EventSet) {
+        // TODO(ptc) revisit error handling, not sure if okay to ignore
+        // errors on readable/writable/closed
         if events.is_readable() {
             match token {
                 SERVER => {
-                    self.accept(event_loop).unwrap()
+                    let _ = self.accept(event_loop);
                 }
                 conn => {
-                    self.conn_readable(event_loop, conn).unwrap()
+                    let _ = self.conn_readable(event_loop, conn);
                 }
             }
         }
 
         if events.is_writable() {
             match token {
-                SERVER => panic!("received writable for SERVER token"),
-                conn => self.conn_writable(event_loop, conn).unwrap(),
+                SERVER => {
+                    println!("received writable for SERVER events: '{:?}' token: '{:?}'", events, token);
+                },
+                conn => {
+                    let _ = self.conn_writable(event_loop, conn);
+                },
+            }
+        }
+
+        if events.is_hup() {
+            match token {
+                SERVER => panic!("listening socket closed underneath us!"),
+                conn => {
+                    let _ = self.conn_closed(event_loop, conn);
+                },
             }
         }
     }
@@ -117,39 +136,55 @@ impl MioServer {
     }
 
     /// Accept a pending connection on the listening socket
-    pub fn accept(&mut self, event_loop: &mut EventLoop<Self>) -> io::Result<()> {
-        // TODO(ptc) proper error handling
-        let sock = self.sock.accept().unwrap().unwrap();
+    pub fn accept(&mut self, event_loop: &mut EventLoop<Self>) -> Result<()> {
+        let res = try!(self.sock.accept());
+        let sock = try!(res.ok_or(Error::new(ErrorKind::WouldBlock,
+                                             "Accepting would block")));
         let conn = MioConn::new(sock);
+        // TODO(ptc) proper error handling...
         let tok = self.conns.insert(conn)
             .ok().expect("could not add connection to slab");
 
         // Register the connection
         // TODO(ptc) need a timeout on the connection handshake...
         self.conns[tok].token = Some(tok);
-        event_loop.register_opt(&self.conns[tok].sock, tok, EventSet::readable(), PollOpt::edge())
+        event_loop.register_opt(&self.conns[tok].sock, tok, EventSet::readable() | EventSet::hup(), PollOpt::edge() | PollOpt::oneshot())
             .ok().expect("could not register socket with event loop");
 
         Ok(())
     }
 
     /// Handle when a connection is readable
-    fn conn_readable(&mut self, event_loop: &mut EventLoop<Self>, tok: Token) -> io::Result<()> {
-        self.conn(tok).readable(event_loop)
+    fn conn_readable(&mut self, event_loop: &mut EventLoop<Self>, tok: Token) -> Result<()> {
+        match self.conns.get_mut(tok) {
+            // Events delivered for already closed connection
+            None => Ok(()),
+            Some(conn) => conn.readable(event_loop),
+        }
     }
 
     /// Handle when a connection is writable
-    fn conn_writable(&mut self, event_loop: &mut EventLoop<Self>, tok: Token) -> io::Result<()> {
-        self.conn(tok).writable(event_loop)
+    fn conn_writable(&mut self, event_loop: &mut EventLoop<Self>, tok: Token) -> Result<()> {
+        match self.conns.get_mut(tok) {
+            // Events delivered for already closed connection
+            None => Ok(()),
+            Some(conn) => conn.writable(event_loop),
+        }
     }
 
-    /// Access a specific connection
-    fn conn<'a>(&'a mut self, tok: Token) -> &'a mut MioConn {
-        &mut self.conns[tok]
-}
+    /// Handle when a connection is hung up/closes
+    fn conn_closed(&mut self, event_loop: &mut EventLoop<Self>, tok: Token) -> Result<()> {
+        let res = match self.conns.get_mut(tok) {
+            // Events delivered for already closed connection
+            None => Ok(()),
+            Some(conn) => conn.closed(event_loop),
+        };
+        self.conns.remove(tok);
+        res
+    }
 
-  pub fn run(&mut self, event_loop : &mut EventLoop<Self>) {
-      event_loop.register_opt(&self.sock, SERVER, EventSet::readable(), PollOpt::edge()).ok().expect("Unable to register server socket with event loop");
-      event_loop.run(self).ok();
-  }
+    pub fn run(&mut self, event_loop : &mut EventLoop<Self>) {
+        event_loop.register_opt(&self.sock, SERVER, EventSet::readable(), PollOpt::edge()).ok().expect("Unable to register server socket with event loop");
+        event_loop.run(self).ok();
+    }
 }
